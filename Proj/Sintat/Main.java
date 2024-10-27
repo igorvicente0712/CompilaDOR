971910f80e47a989be6a02d485b7ee7ac16db4bd
Igor Vicente <igorvicente0712@hotmail.com>
2024-10-27 19:43:47 -0300

    ajuste parser

--- Proj/Sintat/Main.java
+++ Proj/Sintat/Main.java
@@ -12,15 +12,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Token> tokens = new ArrayList<>();
-        tokens.add(new Token ("reservada_if", "if"));
+        tokens.add(new Token ("reservada_if", "se"));
         tokens.add(new Token("id", "soma"));
         tokens.add(new Token("operador_condicional", ">"));
         tokens.add(new Token("num", "5"));
-        tokens.add(new Token("reservada_then", "then"));
+        tokens.add(new Token("reservada_then", "entao"));
         tokens.add(new Token("id", "soma"));
         tokens.add(new Token("operador_atribuicao", "="));
         tokens.add(new Token("num", "3"));
-        tokens.add(new Token("reservada_else", "else"));
+        tokens.add(new Token("reservada_else", "ou"));
         tokens.add(new Token("id", "soma"));
         tokens.add(new Token("operador_atribuicao", "="));
         tokens.add(new Token("num", "2"));
